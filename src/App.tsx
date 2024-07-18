896c11772f114aabb9509a9bcb54d2dec0134b40
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-07-18 06:30:31 +0100

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Provider } from "react-redux";
 
 import { store } from "./state";
-import RepositoriesList from "./components/RepositoriesList";
 
 function App() {
   return (
     <Provider store={store}>
       <div>
         <h1>Search for a Package</h1>
-        <RepositoriesList />
       </div>
     </Provider>
   );
