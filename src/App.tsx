ec6c2fbcee16402199e650aa9812057ee319be16
Tzvetan Marinov <tmarinov@stamus-networks.com>
2024-07-18 07:56:19 +0100

    useActions hook for easier Dispatching of Action Creators

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Provider } from "react-redux";
 
 import { store } from "./state";
+import RepositoriesList from "./components/RepositoriesList";
 
 function App() {
   return (
     <Provider store={store}>
       <div>
         <h1>Search for a Package</h1>
+        <RepositoriesList />
       </div>
     </Provider>
   );
